0278e2f718d8a2d729638915f2bff658585156e3
jalepi <jalepi@live.com>
2025-12-21 12:04:10 +0100

    Update packages (#12)
    * simple pnpm update
    
    * upgrade vitest to version 4

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "vitest/config";
-import workspace from "./vitest.workspace.mjs";
 
 export default defineConfig(({ mode }) => ({
   test: {
-    workspace,
+    projects: ["packages/*"],
     environment: "happy-dom",
     reporters: mode === "ci" ? ["default", "junit"] : ["default"],
     outputFile: {
