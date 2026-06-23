9197c272ce87679ad9e556bdddb53b48427a25b1
Jalepi <jalepi@live.com>
2026-06-23 21:17:33 +0200

    decorate cleanup

--- packages/decor/src/async/index.test.ts
+++ packages/decor/src/async/index.test.ts
@@ -6,7 +6,6 @@ describe("decor async package exports tests", () => {
     expect(index).toBeDefined();
     expect(index).toMatchObject({
       ...(await import("./attempt")),
-      ...(await import("./decorate")),
       ...(await import("./decorator")),
       ...(await import("./middleware")),
       ...(await import("./probe")),
--- packages/decor/src/index.ts
+++ packages/decor/src/index.ts
@@ -1,7 +1,5 @@
-import { attempt } from "./attempt";
-import { decorate } from "./decorate";
-import { decorator } from "./decorator";
-import { middleware } from "./middleware";
-import { probe } from "./probe";
-
-export { attempt, decorate, decorator, middleware, probe };
+export { attempt } from "./attempt";
+export { decorate } from "./decorate";
+export { decorator } from "./decorator";
+export { middleware } from "./middleware";
+export { probe } from "./probe";
